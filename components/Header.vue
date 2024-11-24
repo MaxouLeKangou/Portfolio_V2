1826c6fb4cdf75fb179fc977b530909988d4eafe
MaxouLeKangou <maxime.legrand@bessarion.fr>
2024-11-24 20:55:13 +0100

    feat(Header): Create Header component

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="w-full mt-5 flex justify-between">
+		<p class="w-10 h-10">
+			<component
+				:is="route.path === '/' ? 'span' : nuxtlink"
+				:to="route.path === '/' ? undefined : '/'"
+			>
+				<NuxtImg src="/img/logo.webp" class="rounded-full"/>
+			</component>
+		</p>
+
+		<button>
+			<p class="text-base">toggleMenu<span class="text-white-300">( )</span></p>
+		</button>
+    </header>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const nuxtlink = resolveComponent('NuxtLink');
+
+const menuVisible = ref(false);
+const toggleMenu = () => {
+	menuVisible.value = !menuVisible.value;
+};
+</script>
