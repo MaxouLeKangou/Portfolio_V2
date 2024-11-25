063f64808ee93bb6c731db0bfa215acb166e780c
MaxouLeKangou <maxime.legrand@bessarion.fr>
2024-11-25 18:20:59 +0100

    feat(Skills): Create component HomeSkills

---
+++ components/Home/Skills.vue
@@ -0,0 +1,14 @@
+<template>
+	<section>
+		<SliceZone wrapper="main" :slices="document?.data?.slices ?? []" :components="components"/>
+	</section>
+</template>
+
+<script setup lang="ts">
+import { components } from "~/slices";
+
+const prismic = usePrismic();
+const { data: document } = useAsyncData('skills', () =>
+  	prismic.client.getSingle('skills')
+);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-    <main class="flex flex-col gap-24">
+    <main class="flex flex-col gap-40">
         <HomeHero/>
         
-        <section id="about">
+        <section id="about" class="flex flex-col gap-24">
             <BlocContent title="About me" content="Hey! I’m Maxime, twenty years old {job}. I make beautiful websites that will help achieve your goals!" letTitle="job" letValue="front-end developer"/>
+            <HomeSkills/>
         </section>
     </main>
 </template>
