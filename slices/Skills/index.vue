caf64c5f0de4f7fce6573adfa9c67a3e35c677ee
Maxime LEGRAND <maxime.legrand@bessarion.fr>
2024-11-27 20:58:08 +0100

    feat(Responsive): slice Skills

--- slices/Skills/index.vue
+++ slices/Skills/index.vue
@@ -12,10 +12,10 @@ defineProps(
 </script>
 
 <template>
-    <ul :data-slice-type="slice.slice_type" :data-slice-variation="slice.variation" class="grid grid-cols-2 gap-3">
-        <li v-for="skill of slice.primary.skills" class="relative flex flex-col justify-center gap-4 items-center h-28 w-full bg-background-200 rounded-xl">
-            <NuxtImg :src="skill.icon.url ?? ''" class="w-9 h-9"/>
-            <h4 class="uppercase text-sm tracking-first">
+    <ul :data-slice-type="slice.slice_type" :data-slice-variation="slice.variation" class="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 max-w-[1000px] mx-auto">
+        <li v-for="skill of slice.primary.skills" class="relative flex flex-col justify-center gap-4 items-center h-28 w-full bg-background-200 rounded-xl lg:h-36">
+            <NuxtImg :src="skill.icon.url ?? ''" class="w-9 h-9 lg:w-14 lg:h-14"/>
+            <h4 class="uppercase text-sm tracking-first lg:text-desk-sm">
                 <nuxt-link :to="skill.link ?? '#'" class="before:content-[''] before:absolute before:inset-0">{{ skill.name }}</nuxt-link>
             </h4>
         </li>
