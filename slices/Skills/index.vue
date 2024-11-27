a7fe74f93c538bb97a34ae333deb43ec88847afd
Maxime LEGRAND <maxime.legrand@bessarion.fr>
2024-11-27 21:00:28 +0100

    fix(Responsive): gap between elements Skills

--- slices/Skills/index.vue
+++ slices/Skills/index.vue
@@ -12,7 +12,7 @@ defineProps(
 </script>
 
 <template>
-    <ul :data-slice-type="slice.slice_type" :data-slice-variation="slice.variation" class="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 max-w-[1000px] mx-auto">
+    <ul :data-slice-type="slice.slice_type" :data-slice-variation="slice.variation" class="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 max-w-[1000px] mx-auto lg:gap-5">
         <li v-for="skill of slice.primary.skills" class="relative flex flex-col justify-center gap-4 items-center h-28 w-full bg-background-200 rounded-xl lg:h-36">
             <NuxtImg :src="skill.icon.url ?? ''" class="w-9 h-9 lg:w-14 lg:h-14"/>
             <h4 class="uppercase text-sm tracking-first lg:text-desk-sm">
