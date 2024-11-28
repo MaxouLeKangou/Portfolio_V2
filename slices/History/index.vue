75335b356dfa917f1638746f57b7a19408d2176f
Maxime LEGRAND <maxime.legrand@bessarion.fr>
2024-11-28 18:59:35 +0100

    fix(Reponsive): component History

--- slices/History/index.vue
+++ slices/History/index.vue
@@ -21,7 +21,7 @@ function formatDate(date: string) {
 
 <template>
     <ul :data-slice-type="slice.slice_type" :data-slice-variation="slice.variation" class="md:grid md:grid-cols-2 md:gap-5 max-w-[1000px] mx-auto">
-        <li v-for="history of slice.primary.history" class="relative flex items-center gap-5 w-full h-28 px-3 md:border-b md:border-white-300/25 lg:h-40 lg:px-5">
+        <li v-for="history of slice.primary.history" class="relative flex items-center gap-5 w-full h-28 px-3 sm:border-b sm:border-white-300/25 lg:h-40 lg:px-5">
             <div class="flex justify-center items-center w-14 h-14 bg-background-200 rounded-xl lg:w-20 lg:h-20">
                 <NuxtImg :src="history.icon.url ?? ''" class="w-7 h-7 lg:w-10 lg:h-10"/>
             </div>
@@ -45,6 +45,6 @@ function formatDate(date: string) {
 
 <style scoped>
 li::before {
-    @apply content-[''] absolute w-screen h-px left-[-1.25rem] bottom-0 bg-white-300/25 sm:left-[-4rem] md:content-none
+    @apply content-[''] absolute w-screen h-px left-[-1.25rem] bottom-0 bg-white-300/25 sm:content-none
 }
 </style>
