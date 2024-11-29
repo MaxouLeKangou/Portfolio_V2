4a2132be98ec7ef49c620f1c456ada787c3ab1bc
Maxime LEGRAND <maxime.legrand@bessarion.fr>
2024-11-29 22:41:17 +0100

    feat(Animation): create custom animation

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <header class="w-full h-10 mt-5 flex items-center justify-between md:mt-10 lg:h-16">
-		<p class="w-10 h-10 lg:w-16 lg:h-16">
+		<p class="w-10 h-10 transition duration-500 lg:w-16 lg:h-16" :class="{'hover:scale-110': route.path !== '/'}">
 			<component
 				:is="route.path === '/' ? 'span' : nuxtlink"
 				:to="route.path === '/' ? undefined : '/'"
 			>
-				<NuxtImg src="/img/logo.webp" class="rounded-full" alt="Maxime LEGRAND portfolio"/>
+				<NuxtImg src="/img/logo.webp" class="h-full aspect-square rounded-full" alt="Maxime LEGRAND portfolio"/>
 			</component>
 		</p>
 
--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
 	<button aria-label="close menu" @click="closeMenu" class="fixed z-40 w-screen inset-0 bg-[#000000]/50 transition-all duration-300" :class="isVisible ? 'translate-x-0' : 'translate-x-full'"></button>
-	<section class="fixed z-50 w-96 max-w-full top-0 right-0 bottom-0 flex flex-col bg-background-100 pt-5 pr-5 pb-10 pl-10 transition-all duration-300 lg:w-[500px]" :class="isVisible ? 'translate-x-0' : 'translate-x-full'">
+	<section class="fixed z-50 w-96 max-w-full top-0 right-0 bottom-0 flex flex-col bg-background-100 pt-5 pr-5 pb-10 pl-10 transition-all duration-300 lg:w-[500px] lg:pt-10 lg:pr-10 lg:pb-14" :class="isVisible ? 'translate-x-0' : 'translate-x-full'">
 		<div class="flex-grow">
 			<div class="flex gap-2 flex-wrap justify-between items-center h-10">
 				<p>
@@ -9,16 +9,17 @@
 					</NuxtLink>
 				</p>
 				<button @click="closeMenu">
-					<p class="text-base text-white-300">menuVisible = <span class="animate-pulse text-white-100 font-medium">true</span></p>
+					<p class="text-base text-white-300 lg:text-desk-sm">menuVisible = <span class="animate-pulse text-white-100 font-medium">true</span></p>
 				</button>
 			</div>
-			<div class="my-16 flex flex-col gap-3">
+			<div class="my-16 text-base flex flex-col gap-3 lg:text-desk-sm">
 				<span class="text-white-300">const menu = [</span>
-				<nav class="pl-5">
-					<ul class="flex flex-col gap-3 text-lg font-semibold">
+				<nav class="pl-5 lg:pl-8">
+					<ul class="animation inline-flex flex-col gap-3 text-lg font-semibold lg:text-xl">
 						<li>
 							<p>
 								<component
+									@click="route.path !== '/' ?? closeMenu"
 									:is="route.path === '/' ? 'span' : nuxtlink"
 									:to="route.path === '/' ? undefined : '/'"
 								>
@@ -29,6 +30,7 @@
 						<li>
 							<p>
 								<component
+									@click="route.path !== '/works' ?? closeMenu"
 									:is="route.path === '/works' ? 'span' : nuxtlink"
 									:to="route.path === '/works' ? undefined : '/works'"
 								>
@@ -39,6 +41,7 @@
 						<li>
 							<p>
 								<component
+									@click="route.path !== '/secret' ?? closeMenu"
 									:is="route.path === '/secret' ? 'span' : nuxtlink"
 									:to="route.path === '/secret' ? undefined : '/secret'"
 								>
@@ -56,7 +59,7 @@
 			<span class="mt-0.5">
 				<LazyIconsCheck class="text-primary"/>
 			</span>
-			<div class="flex flex-col gap-1.5 text-base">
+			<div class="flex flex-col gap-1.5 text-base lg:text-desk-sm">
 				<p class="text-primary font-semibold tracking-first">WEBSITE</p>
 				<div class="flex flex-col text-white-300 font-light">
 					<p>Successfully built website at</p>
